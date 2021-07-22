8b8d2bf915720f45617afe26d0b0f8deae6be150
Enzo Notario <enzo.notario@gmail.com>
2021-07-22 20:47:45 +0000

    - simplificar algoritmo - utilizar  en vez de

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const prompts = require('prompts'); // Incluímos la librería.
+const prompt = require('prompt-sync') // Incluímos la librería.
 
-(async () => {
-  // Le preguntamos al usuario el número A.
-  const A = await prompts({
-    type: 'number',
-    name: 'value',
-    message: 'Ingresa el valor de A',
-  });
+// Le preguntamos al usuario el número A.
+var A = prompt('Ingresa el valor de A')
 
-  // Le preguntamos al usuario el número B.
-  const B = await prompts({
-    type: 'number',
-    name: 'value',
-    message: 'Ingresa el valor de B',
-  });
+// Le preguntamos al usuario el número B.
+var B = prompt('Ingresa el valor de B')
 
-  // Realizamos la suma.
-  const C = A.value + B.value;
+// Realizamos la suma.
+var C = A + B
 
-  // Mostramos el resultado de la suma de A y B.
-  console.log(`La suma de ${A.value} + ${B.value} es ${C}`);
-})();
+// Mostramos el resultado de la suma de A y B.
+console.log(`La suma de ${A} + ${B} es ${C}`)
