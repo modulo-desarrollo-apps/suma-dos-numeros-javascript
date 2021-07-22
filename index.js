a56d926d9cdb7fada9ac6a8ba2ef6b9e91d1792c
Enzo Notario <enzo.notario@gmail.com>
2021-07-22 21:39:21 +0000

    - fix: libreria

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const prompt = require('prompt-sync') // Incluímos la librería.
+const prompt = require('prompt-sync')() // Incluímos la librería.
 
 // Le preguntamos al usuario el número A.
-var A = prompt('Ingresa el valor de A')
+var A = prompt('Ingresa el valor de A: ')
 
 // Le preguntamos al usuario el número B.
-var B = prompt('Ingresa el valor de B')
+var B = prompt('Ingresa el valor de B: ')
 
 // Realizamos la suma.
-var C = A + B
+var C = Number(A) + Number(B)
 
 // Mostramos el valor de la variable C (que contiene la suma de A y B).
 console.log(`La suma de ${A} + ${B} es ${C}`)
